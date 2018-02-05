29aa8b147209c1ef9d6d74c68044f2f043b8b233
ferran.verdes <ferran.verdes@bonarea.com>
2018-02-05 12:40:01 +0100

    Added component lifecycle file

---
+++ react/component-lifecycle.js
@@ -0,0 +1,44 @@
+classMiComponenteextendsComponents {
+
+	constructor(){
+	// Enlazo (bind) eventos y/o inicializo estado
+	}
+	componentWillMount(){
+	// Se ejecuta antes de montar el componente
+	// Se podría usar para hacer un setState()
+	}
+	render(){
+	// Contiene todos los elementos a renderizar
+	// podrías usarlo para calcular propiedades (ej: concatenar una cadena)
+	}
+	componentDidMount(){
+	//Solo se lanza una vez
+	//Ideal para llamar a una API, hacer un setInteval, etc
+	}
+
+	//Actualización:
+	componentWillReceiveProps(){
+	//Es llamado cuando el componente recibe nuevas propiedades.
+
+	}
+	shouldComponentUpdate(){
+	//Idea para poner una condición y  si las propiedades que le llegaron anteriormente
+	// eran las mismas que tenia retornar false para evitar re-renderear el componente
+	}
+	componentWillUpdate(){
+	//metodo llamado antes de re-renderizar el componente si shouldComponentUpdate devolvió true
+	}
+
+	// re-render si es necesario...
+
+	componentDidUpdate(){
+	//Método llamado luego del re-render
+	}
+	componentWillUnmount(){
+	//Método llamado antes de desmontar el componente
+	}
+	componentDidCatch(){
+	// Si ocurre algún error, lo capturo desde acá:
+	}
+
+}
