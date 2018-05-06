9513f9319e46ef083a75b47fcc29060c3d1efe11
ferran.verdes <ferran.verdes@bonarea.com>
2018-05-06 12:39:40 +0200

    test

--- javascript/pass-by-value-or-reference.js
+++ javascript/pass-by-value-or-reference.js
@@ -41,7 +41,7 @@ console.log('After call first: ', primitiveValue, object, '\n');
 
 /**
 
-	RAM Memory
+RAM Memory
    _______________ _______________ _______________ _______________ _______________
   |               |               |               |               |               |
   |               |               |               |               |               |
@@ -107,7 +107,7 @@ console.log('After call second: ', primitiveValue, object, '\n');
 
 /**
 
-	RAM Memory
+RAM Memory
    _______________ _______________ _______________ _______________ _______________
   |               |               |               |               |               |
   |               |               |               |               |               |
@@ -177,7 +177,7 @@ console.log('After call third: ', primitiveValue, object, '\n');
 
 /**
 
-	RAM Memory
+RAM Memory
    _______________ _______________ _______________ _______________ _______________
   |               |               |               |               |               |
   |               |               |               |               |               |
@@ -214,7 +214,7 @@ D |               |      Doe      |               |               |             
 	Function body:
 	primitive = 20;					C1 '10' change for '20'
 	obj.subObject = {
-		name: 'Doe'						B1 --> subObject B2 --> B2 'name B3' change for 'name D2'
+		name: 'Doe'				B1 --> subObject B2 --> B2 'name B3' change for 'name D2'
 	};
 
 	After:
@@ -248,7 +248,7 @@ console.log('After call fourth: ', primitiveValue, object, '\n');
 
 /**
 
-	RAM Memory
+RAM Memory
    _______________ _______________ _______________ _______________ _______________
   |               |               |               |               |               |
   |               |               |               |               |               |
@@ -286,7 +286,7 @@ D |               |               |               |               |             
 	subObject *B2* A4
 	primitive = 20;					C1 '10' change for '20'
 	subObject = {
-		name: 'Doe'						subObject 'B2' change for 'A4'
+		name: 'Doe'				subObject 'B2' change for 'A4'
 	};
 
 	After:
@@ -318,7 +318,7 @@ console.log('After call fifth: ', primitiveValue, object, '\n');
 
 /**
 
-	RAM Memory
+RAM Memory
    _______________ _______________ _______________ _______________ _______________
   |               |               |               |               |               |
   |               |               |               |               |               |
@@ -354,8 +354,8 @@ D |               |               |               |               |             
 
 	Function body:
 	subObject B2
-	primitive = 20;						C1 '10' change for '20'
-	subObject.name = 'Doe';		B2 --> name B3 --> B3 'John' change for 'Doe'
+	primitive = 20;			C1 '10' change for '20'
+	subObject.name = 'Doe';			B2 --> name B3 --> B3 'John' change for 'Doe'
 
 	After:
 	primitiveValue A1
@@ -386,7 +386,7 @@ console.log('After call sixth: ', primitiveValue, object, '\n');
 
 /**
 
-	RAM Memory
+RAM Memory
    _______________ _______________ _______________ _______________ _______________
   |               |               |               |               |               |
   |               |               |               |               |               |
@@ -422,8 +422,8 @@ D |               |               |               |               |             
 
 	Function body:
 	name B3
-	primitive = 20;						C1 '10' change for '20'
-	name = 'Doe';							B3 'John' change for 'Doe'
+	primitive = 20;				C1 '10' change for '20'
+	name = 'Doe';					B3 'John' change for 'Doe'
 
 	After:
 	primitiveValue A1
